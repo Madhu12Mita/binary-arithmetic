0d4ddd029dfea785d59e72a337318d886c1193ac
ravathu-jahnavi <124995155+ravathu-jahnavi@users.noreply.github.com>
2023-02-10 15:09:47 +0530

    Update binary-arithimetic.py

--- binary-arithimetic.py
+++ binary-arithimetic.py
@@ -4,7 +4,6 @@ def decToBinary(num,list):
     if num>=1:
         decToBinary(num//2,list)
     list.append(num%2)
-    return list
     
 def add(l1,l2):
     pass
@@ -20,3 +19,17 @@ l2 = []
 
 a = int(input("first num: "))
 b = int(input("second num: "))
+
+decToBinary(a,l1)
+decToBinary(b,l2)
+
+n = int(input("enter 1 for add, 2 for sub, 3 for mul"))
+
+if(n==1):
+    add(l1,l2)
+elif(n==2):
+    sub(l1,l2)
+elif(n==3):
+    mul(l1,l2)
+else:
+    print("enter proper input")
